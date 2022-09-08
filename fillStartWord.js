d31b682bb8e7f0471ba3a6594be7c0e13bd61be3
Kamolphon <kamolphon.thon@kmutt.ac.th>
2022-09-09 02:26:20 +0700

    Test1

--- fillStartWord.js
+++ fillStartWord.js
@@ -1,6 +1,14 @@
 const { template } = require('@babel/core')
 
 function fillStartWord(startWord, word) {
-  //code here
+    if(word === null || word === undefined ){
+    return undefined
+    }
+    if (word.includes(startWord)) {
+        return word
+    }
+    else {
+        return startWord + word
+    }
 }
-module.exports = fillStartWord
+module.exports = fillStartWord 
